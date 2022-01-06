9b590544cb7dc3f17751e8e964367be6774718d3
Talha-Mushtaq <53311657+Talha-Mushtaq@users.noreply.github.com>
2022-01-06 19:54:03 +0500

    changes

--- src/functions/applicationPasswords/readWithId/handler.ts
+++ src/functions/applicationPasswords/readWithId/handler.ts
@@ -17,74 +17,10 @@ import btoa from 'btoa';
 const readWithId: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event,
 ) => {
-  try {
-    const { userId, context } = event.body;
-    const { username, password, url } = event.queryStringParameters;
-    const token = btoa(`${username}:${password}`);
-
-    if (
-      userId === undefined ||
-      context === undefined ||
-      userId === '' ||
-      context === ''
-    )
-      return formatJSONResponse(400, {
-        message: 'Missing required request parameters <userId,context>',
-      });
-
-    if (
-      username === undefined ||
-      password === undefined ||
-      url === undefined ||
-      username === '' ||
-      password === '' ||
-      url === ''
-    ) {
-      return formatJSONResponse(403, {
-        message: 'Fobidden',
-      });
-    }
-
-    let ax = axios.create({
-      baseURL: `${url}`,
-      headers: {
-        Authorization: `Basic ${token}`,
-      },
-    });
-
-    let response = await ax.get(`users/${userId}/application-passwords`, {
-      params: { context: context },
-    });
-
-    return formatJSONResponse(200, {
-      data: response.data,
-    });
-  } catch (err) {
-    if (err.response.status === 404)
-      return formatJSONResponse(404, {
-        message: 'Not Found',
-      });
-    else if (err.response.status === 409)
-      return formatJSONResponse(403, {
-        message: 'Fobidden',
-      });
-    else if (
-      err.response.data.error === 'INVALID_USERNAME' ||
-      err.response.data.error === 'INVALID_PASSWORD' ||
-      err.response.status === 401
-    )
-      return formatJSONResponse(401, {
-        message: 'Unathorized',
-      });
-    else if (err.response.status === 400)
-      return formatJSONResponse(400, {
-        message: 'Invalid parameter <context>',
-      });
-    else
-      return formatJSONResponse(500, {
-        message: 'Internal Server Error',
-      });
-  }
+  return formatJSONResponse({
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    event,
+  });
 };
 
 export const main = middyfy(readWithId);
